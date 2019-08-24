064d4ceae11e7718417c612202bbb6693f07065a
Matthew Pennell <github@matthewpennell.com>
2019-08-24 15:31:50 +0100

    Mobile style improvements.

--- community-links.php
+++ community-links.php
@@ -5,28 +5,24 @@
     <ul>
         <li>
             <a class="discord-widget" href="https://discord.gg/rJnj8JQ" title="Join the Alliance on Discord">
-                <img src="https://discordapp.com/api/guilds/505312121479757824/embed.png?style=banner2">
+                <img src="https://discordapp.com/api/guilds/505312121479757824/embed.png?style=banner2" alt="Zandalar Tribe Alliance Discord">
             </a>
-            Alliance Faction Discord
         </li>
         <li>
             <a class="discord-widget" href="https://discord.gg/dgrCqsB" title="Join us on Discord">
-                <img src="https://discordapp.com/api/guilds/611250505603350530/embed.png?style=banner2">
+                <img src="https://discordapp.com/api/guilds/611250505603350530/embed.png?style=banner2" alt="Zandalar Tribe Neutral Discord">
             </a>
-            Server Discord
         </li>
         <li>
             <a class="discord-widget" href="https://discord.gg/9tKtCmj" title="Join the Horde on Discord">
-                <img src="https://discordapp.com/api/guilds/589202224492576799/embed.png?style=banner2">
+                <img src="https://discordapp.com/api/guilds/589202224492576799/embed.png?style=banner2" alt="Zandalar Tribe Horde Discord">
             </a>
-            Horde Faction Discord
         </li>
         <li>
             <a href="https://www.reddit.com/r/ZandalarTribe/" class="reddit-widget">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/subreddit.png">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/subreddit.png" alt="Zandalar Tribe Subreddit">
                 r/ZandalarTribe
             </a>
-            Community Subreddit
     </ul>
 
 </div>
